334a6650d34bd976a759b1d091dc7ed53f05c8db
Steve Kieffer <sk@skieffer.info>
2023-01-01 13:48:22 -0500

    Update copyright year

--- tests/test-root/conf.py
+++ tests/test-root/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'sphinx-proofscape test doc'
-copyright = '2022, author'
+copyright = '2022-2023, author'
 author = 'author'
 
 # -- General configuration ---------------------------------------------------
